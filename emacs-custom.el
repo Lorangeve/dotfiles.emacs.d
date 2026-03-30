57baf5778406135f887b8b5f62729ff9692874c0
lorangeve <lorangeve@gmail.com>
2026-03-30 11:18:19 +0800

    init

---
+++ emacs-custom.el
@@ -0,0 +1,17 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ace-window amx company-box counsel doom-themes fanyi geiser-guile
+		liberime lsp-scheme org-download org-modern paredit
+		rainbow-delimiters rime transpose-frame undo-tree
+		which-key-posframe)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
