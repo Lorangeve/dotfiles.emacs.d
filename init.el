6e7b0a1ec874bcc1a47a99be33f299f032a8876b
lorangeve <lorangeve@gmail.com>
2026-03-30 15:58:48 +0800

    加了输入法进去

--- init.el
+++ init.el
@@ -1,4 +1,7 @@
-(global-display-line-numbers-mode)  ; （可选）显示相对行号
+(global-display-line-numbers-mode)  ;（可选）显示相对行号
+(global-set-key (kbd "C-j") nil) 
+(global-set-key (kbd "<f4>") nil)
+
 (setq inhibit-startup-screen t)
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
@@ -60,6 +63,29 @@
   :ensure t
   :init
   (global-company-mode))
+
+(use-package rime
+  :ensure t
+  :custom
+  (default-input-method "rime")
+  ;; 如果你是 MacOS 且使用 Squirrel，或者 Linux 使用ibus/fcitx，
+  ;; 通常需要指定 librime 的共享库位置（以下为常见路径，需根据系统调整）
+  (rime-librime-root "~/.local/share/fcitx5/rime")
+  :config
+  ;; (setq rime-show-candidate 'posframe) ;; 如果安装了 posframe，候选框会更好看
+  ;; 常用快捷键：进入 RIME 菜单（选方案、切换简繁）
+  :bind
+  (:map rime-mode-map
+        ("C-`" . rime-send-keybinding))) ;; 这里的 C-` 是为了呼出 RIME 的方案选单
+
+(setq custom-safe-themes t)
+(use-package smart-mode-line
+  :ensure t
+  :config
+  ;; 显式设置主题，避免它乱猜
+  ;; 如果用暗色主题，就把 'light 改成 'dark 或 'respectful
+  (setq sml/theme 'light) 
+  (sml/setup))
 
 (use-package ace-window
   :ensure t
