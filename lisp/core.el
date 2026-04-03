cddc70cff1a35366750c1181d8b0ec8b25805dd9
lorangeve <lorangeve@gmail.com>
2026-04-03 10:01:11 +0800

    防止初次打开时因为缩放倍率切换导致的抖动

--- lisp/core.el
+++ lisp/core.el
@@ -1,17 +1,20 @@
 ;;; core.el --- 基础界面与编辑习惯 -*- lexical-binding: t -*-
 
-;; 将备份文件集中存放到 ~/.emacs.d/backups 目录下
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+;; ;; 将备份文件集中存放到 ~/.emacs.d/backups 目录下
+;; (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
-;; 将自动保存文件集中存放到 ~/.emacs.d/autosave 目录下
-(setq auto-save-file-name-transforms
-      '((".*" "~/.emacs.d/autosave/" t)))
+;; ;; 将自动保存文件集中存放到 ~/.emacs.d/autosave 目录下
+;; (setq auto-save-file-name-transforms
+;;       '((".*" "~/.emacs.d/autosave/" t)))
 
-;; 创建目录（如果不存在）
-(unless (file-exists-p "~/.emacs.d/backups")
-  (make-directory "~/.emacs.d/backups"))
-(unless (file-exists-p "~/.emacs.d/autosave")
-  (make-directory "~/.emacs.d/autosave"))
+;; ;; 创建目录（如果不存在）
+;; (unless (file-exists-p "~/.emacs.d/backups")
+;;   (make-directory "~/.emacs.d/backups"))
+;; (unless (file-exists-p "~/.emacs.d/autosave")
+;;   (make-directory "~/.emacs.d/autosave"))
+
+;; 防止初次打开时因为缩放倍率切换导致的抖动
+(setq frame-inhibit-implied-resize t)
 
 ;; 边栏数字显示
 (global-display-line-numbers-mode)
